2e9305632019a407392b0e3f796abb470166d510
Steven Wang <steven@stevens-macbook-pro.local>
2016-04-13 17:03:29 +0800

    Initial Commit

---
+++ poerty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  poerty
+//
+//  Created by Steven Wang on 2016/4/13.
+//  Copyright © 2016年 ka666wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
